d662f0467542df1ffab0f2cd29eead256db3448f
M0nssef <122165584+M0nssef@users.noreply.github.com>
2023-08-23 16:40:23 +0100

    Add files via upload

---
+++ src/assets/data/portfolioData.js
@@ -0,0 +1,105 @@
+import portfolioImg01 from "../images/Online_Eduction_Image.png";
+import portfolioImg02 from "../images/Grocery_Image.png";
+import portfolioImg03 from "../images/Restaurant_Image.png";
+import portfolioImg04 from "../images/Travel_Image.png";
+import portfolioImg05 from "../images/BlackJack_Game_Image.png";
+import portfolioImg06 from "../images/Lead-traceker_Image.png";
+import portfolioImg07 from "../images/Meme-Gen-Image.png";
+import portfolioImg08 from "../images/Notes-Gen-Image.png";
+import portfolioImg09 from "../images/booking_Image.png";
+
+const portfolios = [
+  {
+    id: "01",
+    imgUrl: portfolioImg01,
+    category: "Web Design",
+    title: "Online Education",
+    description:
+      "Online Education :is an inclusive online education platform offering diverse courses taught by experts. With interactive content and personalized learning paths, learners worldwide can thrive academically and professionally",
+    technologies: ["HTML", "Css", "Javascript", "MongoDB"],
+    siteUrl: "#",
+  },
+  {
+    id: "02",
+    imgUrl: portfolioImg02,
+    category: "Web Design",
+    title: "Grocery",
+    description:
+      "Your Online Grocery Destination offers a vast selection of fresh produce, pantry staples, and household essentials. With user-friendly navigation, doorstep delivery, and personalized recommendations, GroceryEase simplifies shopping. Experience convenience and quality with every click at GroceryEase",
+    technologies: ["Html", "Css", "Javascript", "MongoDB"],
+    siteUrl: "#",
+  },
+  {
+    id: "03",
+    imgUrl: portfolioImg03,
+    category: "Web Design",
+    title: "Restaurant",
+    description:
+      "Explore our thoughtfully crafted menu that celebrates both classic flavors and innovative dishes, all made from the finest seasonal ingredients. Immerse yourself in our inviting ambiance and exceptional service, creating unforgettable moments for every occasion.",
+    technologies: ["Html", "Css", "Javascript", "MongoDB"],
+    siteUrl: "#",
+  },
+  {
+    id: "04",
+    imgUrl: portfolioImg04,
+    category: "Web Design",
+    title: "Travel",
+    description:
+      "Our travel website is your gateway to endless adventures, offering meticulously curated itineraries to breathtaking destinations around the world. From exotic getaways to cultural immersions, let WanderlustExplorer be your guide to creating unforgettable memories and fulfilling your wanderlust.",
+    technologies: ["Html", "Css", "javascript", "MongoDB"],
+    siteUrl: "#",
+  },
+  {
+    id: "06",
+    imgUrl: portfolioImg05,
+    category: "Apps",
+    title: "BlackJack Game",
+    description:
+      "Your Ultimate Blackjack App. AceJack brings the excitement of the casino to your fingertips, offering a sleek and user-friendly platform to enjoy the classic card game.With realistic gameplay, strategic challenges, and interactive features, AceJack is your go-to app for mastering blackjack and testing your skills against players from around the globe",
+    technologies: ["ReactJS", "Css", "Node.js"],
+    siteUrl: "#",
+  },
+  {
+    id: "07",
+    imgUrl: portfolioImg06,
+    category: "Apps",
+    title: "Lead-Tracker",
+    description:
+      "Lead-Tracker empowers your business with cutting-edge lead tracking and management. From capturing prospects to nurturing relationships, our intuitive app streamlines the sales process, ensuring you never miss a valuable opportunity to grow and succeed.",
+    technologies: ["ReactJS", "Css", "Node.js"],
+    siteUrl: "#",
+  },
+  {
+    id: "08",
+    imgUrl: portfolioImg07,
+    category: "Apps",
+    title: "Meme-Generator",
+    description:
+      "The Ultimate Memegenerator App. MemeGenius is your creative companion, offering a vast library of templates, witty captions, and easy-to-use tools to craft hilarious and shareable memes. From pop culture references to personal humor, express yourself and bring laughter to your friends and followers with MemeGenius.",
+    technologies: ["ReactJS", "Tailwind css", "Node.js", "MongoDB"],
+    siteUrl: "#",
+  },
+  {
+    id: "09",
+    imgUrl: portfolioImg08,
+    category: "Apps",
+    title: "Notes-Generator",
+    description:
+      "Your Premier Notes Generator App. NoteGen revolutionizes organization and productivity by offering a seamless platform to create, structure, and customize notes. Whether for study, work, or personal projects, streamline your thoughts and ideas with NoteGenix and experience a new level of note-taking efficiency.",
+    technologies: ["ReactJS", "Css", "Node.js"],
+    siteUrl: "#",
+  },
+  {
+    id: "10",
+    imgUrl: portfolioImg09,
+    category: "Web Design",
+    title: "Booking Website",
+    description:
+      "Your Ultimate Online Booking Platform. BookEase simplifies the way you secure appointments, reservations, and services, offering a user-friendly interface and a comprehensive selection of options. From travel accommodations to spa appointments, unlock convenience and efficiency with BookEase, your go-to booking companion.",
+    technologies: ["Laravel", "Css", "MySQL"],
+    siteUrl: "#",
+  },
+  
+];
+
+export default portfolios;
